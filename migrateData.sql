88de19b441c3867e3f58f1701246b07b6648932f
Zahin Mohammad <zahin.dev@gmail.com>
2020-02-05 21:47:08 -0500

    path change

--- migrateData.sql
+++ migrateData.sql
@@ -1,5 +1,5 @@
-source /home/z5mohamm/ece356-a2/createTables;
-source /home/z5mohamm/ece356-a2/createNewTables;
-source /home/z5mohamm/ece356-a2/migrateTables;
-source /home/z5mohamm/ece356-a2/deleteOldTables;
-source /home/z5mohamm/ece356-a2/createViews;
+source ./createTables;
+source ./createNewTables;
+source ./migrateTables;
+source ./deleteOldTables;
+source ./createViews;
