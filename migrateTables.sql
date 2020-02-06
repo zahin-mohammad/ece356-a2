7a52c62d427f37cbed14806b1aeff93cc3d732dc
Zahin Mohammad <zahin.dev@gmail.com>
2020-02-06 14:55:42 -0500

    first name and last name

--- migrateTables.sql
+++ migrateTables.sql
@@ -1,6 +1,12 @@
 INSERT INTO NewEmployee (empID, firstName, middleName, lastName, job, salary) 
     (
-    SELECT DISTINCT Employee.empid, Employee.empName, Employee.empName, Employee.empName, Employee.job, Employee.salary 
+    SELECT DISTINCT 
+    Employee.empid, 
+    REGEXP_SUBSTR(Employee.empName, "^[A-z]+"),
+    REGEXP_SUBSTR(Employee.empName, "[A-z]+"),
+    REGEXP_SUBSTR(Employee.empName, "[A-z]+$"),
+    Employee.job, 
+    Employee.salary 
     FROM Employee 
     );
 
