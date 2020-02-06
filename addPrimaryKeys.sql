c545e2be94ecbc75ea0900ca6a65237d1db83ce0
Zahin Mohammad <zahin.dev@gmail.com>
2020-02-05 20:23:49 -0500

    create new tables

---
+++ addPrimaryKeys.sql
@@ -0,0 +1,46 @@
+alter table Persion add primary key (persionId,Pname,PMID)
+
+CREATE TABLE NewPostalCode(
+    postalCode VARCHAR,
+    cityName VARCHAR,
+    provName VARCHAR
+    PRIMARY KEY(postalCode)
+);
+
+CREATE TABLE NewDepartment(
+    deptID INT,
+    deptName VARCHAR(100),
+    PRIMARY KEY (deptID)
+);
+
+CREATE TABLE NewDepartmentLocation(
+    deptID INT,
+    postalCode VARCHAR,
+    strNumber VARCHAR,
+    strName VARCHAR,
+    PRIMARY KEY (deptID, postalCode),
+    FOFOREIGN KEY (`postalCode`) REFERENCES NewPostalCode(`postalCode`)
+);
+
+-- Assigned stayed the same
+-- Project satyed the same
+
+CREATE TABLE NewEmployee(
+    empID INT,
+    firstName VARCHAR,
+    middleName VARCHAR,
+    lastName VARCHAR,
+    job VARCHAR,
+    salary INT,
+    PRIMARY KEY (empID)
+);
+
+CREATE TABLE NewEmployeeDepartment(
+    empID INT,
+    deptID INT,
+    PRIMARY KEY (empID),
+    FOREIGN KEY (`empID`) REFERENCES NewEmployee(`empID`),
+    FOREIGN KEY (`deptID`) REFERENCES NewDepartment(`deptID`)
+);
+
+
--- createTables.sql
+++ createTables.sql
@@ -7,7 +7,7 @@ CREATE TABLE Department(deptID INT,deptName VARCHAR(100),location VARCHAR(100));
 DROP TABLE IF EXISTS Employee;  
 CREATE TABLE Employee(empID INT,empName VARCHAR(100),job VARCHAR(100),deptID INT,salary INT);
                   
- DROP TABLE IF EXISTS Assigned;             
+DROP TABLE IF EXISTS Assigned;             
 CREATE TABLE Assigned(empID INT,projID INT,role VARCHAR(100));                 
         
 DROP TABLE IF EXISTS Project;           
