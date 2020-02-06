58c711377aa5a7d3135bf9259eec22b582102453
Zahin Mohammad <zahin.dev@gmail.com>
2020-02-05 21:27:57 -0500

    define varchar length

--- createNewTables.sql
+++ createNewTables.sql
@@ -2,23 +2,23 @@ CREATE DATABASE IF NOT EXISTS normalization;
 USE normalization;
 
 CREATE TABLE NewPostalCode(
-    postalCode VARCHAR,
-    cityName VARCHAR,
-    provName VARCHAR,
+    postalCode VARCHAR (100),
+    cityName VARCHAR (100),
+    provName VARCHAR (100),
     PRIMARY KEY(postalCode)
 );
 
 CREATE TABLE NewDepartment(
     deptID INT,
-    deptName VARCHAR(100),
+    deptName VARCHAR (100)(100),
     PRIMARY KEY (deptID)
 );
 
 CREATE TABLE NewDepartmentLocation(
     deptID INT,
-    postalCode VARCHAR,
-    strNumber VARCHAR,
-    strName VARCHAR,
+    postalCode VARCHAR (100),
+    strNumber VARCHAR (100),
+    strName VARCHAR (100),
     PRIMARY KEY (deptID, postalCode),
     FOFOREIGN KEY (`postalCode`) REFERENCES NewPostalCode(`postalCode`)
 );
@@ -28,10 +28,10 @@ CREATE TABLE NewDepartmentLocation(
 
 CREATE TABLE NewEmployee(
     empID INT,
-    firstName VARCHAR,
-    middleName VARCHAR,
-    lastName VARCHAR,
-    job VARCHAR,
+    firstName VARCHAR (100),
+    middleName VARCHAR (100),
+    lastName VARCHAR (100),
+    job VARCHAR (100),
     salary INT,
     PRIMARY KEY (empID)
 );
