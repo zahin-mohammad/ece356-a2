ccd38297701e1169b0487d74adb41acfbdec4b82
Zahin Mohammad <zahin.dev@gmail.com>
2020-02-05 21:58:32 -0500

    migrate NewEmployeeDepartment

--- migrateTables.sql
+++ migrateTables.sql
@@ -3,3 +3,9 @@ INSERT INTO NewEmployee (empID, firstName, middleName, lastName, job, salary)
     SELECT DISTINCT Employee.empid, Employee.empName, Employee.empName, Employee.empName, Employee.job, Employee.salary 
     FROM Employee 
     );
+
+INSERT INTO NewEmployeeDepartment (empID, deptID) 
+    (
+    SELECT DISTINCT Employee.empid, Employee.deptID
+    FROM Employee 
+    );
