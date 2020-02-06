51c8eda7e6d57b00047b0b3b22b90bca34fc8f48
Zahin Mohammad <zahin.dev@gmail.com>
2020-02-06 13:25:35 -0500

    format

--- migrateTables.sql
+++ migrateTables.sql
@@ -4,6 +4,22 @@ INSERT INTO NewEmployee (empID, firstName, middleName, lastName, job, salary)
     FROM Employee 
     );
 
+INSERT INTO NewDepartment (deptID, deptName) 
+    (
+    SELECT DISTINCT Department.deptID, Department.deptName
+    FROM Department 
+    );
+-- regex for street number ([1-9][^\s,A-z]\S)
+-- regex for street name ([^1-9\s][A-z]+\s[A-z]+)
+INSERT INTO NewPostalCode (postalCode, cityName, provName) 
+    (
+    SELECT DISTINCT 
+    REGEXP_SUBSTR(Department.location,""),
+    REGEXP_SUBSTR(Department.location, ""),
+    REGEXP_SUBSTR(Department.location, "")
+    FROM Department 
+    );
+
 INSERT INTO NewEmployeeDepartment (empID, deptID) 
     (
     SELECT DISTINCT Employee.empid, Employee.deptID
